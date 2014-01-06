3f399efd77fb10d29407bb63799bb3b43e434b42
Faydednode <kevin.d.fay@gmail.com>
2014-01-06 02:16:37 -0500

    update to angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,27 +1,52 @@
 'use strict';
 angular.module('portfolioApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.projects = {
-      hbo: {
+    $scope.projects = [
+      {
         mainImg: '/images/work.png',
         tools: 'html5, css3, javascript, jQuery',
         images: [ '/images/work.png', '/images/work.png', '/images/work.png' ],
-        description: 'This is the description of HBO',
+        info: 'This is the description of HBO',
         link: 'http://google.com'
       },
-      cmt: {
+      {
         mainImg: '/images/work.png',
         tools: 'cmt html5, css3, javascript, jQuery',
         images: [ '/images/work.png', '/images/work.png', '/images/work.png' ],
-        description: 'This is the description of CMT',
+        info: 'This is the description of CMT',
         link: 'http://cnn.com'
       },
-      cox: {
+      {
         mainImg: '/images/work.png',
         tools: 'cox html5, css3, javascript, jQuery',
         images: [ '/images/work.png', '/images/work.png', '/images/work.png' ],
-        description: 'This is the description of COX',
+        info: 'This is the description of COX',
+        link: 'http://cox.com'
+      },
+      {
+        mainImg: '/images/work.png',
+        tools: 'html5, css3, javascript, jQuery',
+        images: [ '/images/work.png', '/images/work.png', '/images/work.png' ],
+        info: 'This is the description of HBO',
+        link: 'http://google.com'
+      },
+      {
+        mainImg: '/images/work.png',
+        tools: 'cmt html5, css3, javascript, jQuery',
+        images: [ '/images/work.png', '/images/work.png', '/images/work.png' ],
+        info: 'This is the description of CMT',
+        link: 'http://cnn.com'
+      },
+      {
+        mainImg: '/images/work.png',
+        tools: 'cox html5, css3, javascript, jQuery',
+        images: [ '/images/work.png', '/images/work.png', '/images/work.png' ],
+        info: 'This is the description of COX',
         link: 'http://cox.com'
       }
+    ];
+
+    $scope.itemClick = function() {
+      console.log('here');
     };
   });
